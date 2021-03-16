3e4455de8910c5ff341fb64d819ebb72dab8c6c7
Emmy Adolfsén <emmy.adolfsen@gmail.com>
2021-03-16 11:15:28 +0100

    Made changes for adminUI

---
+++ RestApp/Data/Migrations/20210316095713_AddedDateformat.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestApp.Data.Migrations
+{
+    public partial class AddedDateformat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
